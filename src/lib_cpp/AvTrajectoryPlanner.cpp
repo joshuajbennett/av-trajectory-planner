3c8ee5c014f5f1bd64dd9b6534203bc1fa1a9ce3
Josh Bennett <jjbennet@andrew.cmu.edu>
2018-11-26 08:31:11 -0700

    Fixed issue with lib being ignored in git.

---
+++ src/lib_cpp/AvTrajectoryPlanner.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+
+#include "AvTrajectoryPlanner.hpp"
+#include <json/json.h>
+
+namespace av_trajectory_planner
+{
+
+Planner::Planner() {}
+
+Planner::~Planner() {}
+
+} // namespace av_trajectory_planner
