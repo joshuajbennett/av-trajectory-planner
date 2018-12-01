6e81068fa3bb6775df229320004874540672dc04
Josh Bennett <jjbennet@andrew.cmu.edu>
2018-12-01 12:29:09 -0500

    Updated scenario

--- scenarios/simple_trajectories.py
+++ scenarios/simple_trajectories.py
@@ -6,7 +6,7 @@ import math
 planner = av.Planner(av.AvState(0,0,0,0,0), av.AvState(5,1,0,0,0), av.AvParams(1.0,1.0,0.5,4), av.Boundary([av.Point(0.5,0.5), av.Point(-0.5, 0.5), av.Point(-0.5, -0.5), av.Point(-0.5, 0.5)]), 10, 0.1)
 
 obstacle = av.ObstacleStatic()
-obstacle.obs_outline = av.Boundary([av.Point(0.5,0.2), av.Point(-0.5, 0.2), av.Point(-0.5, -0.2), av.Point(-0.5, 0.2)])
+obstacle.outline = av.Boundary([av.Point(0.5,0.2), av.Point(-0.5, 0.2), av.Point(-0.5, -0.2), av.Point(-0.5, 0.2)])
 obstacle.obs_pose = av.Pose(3,0.5, 0.8)
 
 planner.appendObstacleStatic(obstacle)
