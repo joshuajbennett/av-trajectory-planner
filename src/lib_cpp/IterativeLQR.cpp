ae117d0a83cdc0d36e2d4040e18c7654ce9b0597
Ting-Che <tcl326@nyu.edu>
2018-12-05 21:33:01 -0500

    initialized Ricatti Variables

--- src/lib_cpp/IterativeLQR.cpp
+++ src/lib_cpp/IterativeLQR.cpp
@@ -91,45 +91,51 @@ AvTrajectory IterativeLQR::solveTrajectory()
 		// Get x_bar and u_bar desired
 		auto X_bar_desired = X_desired - X_nominal;
 		auto U_bar_desired = U_desired - U_nominal;
-
-		// Initialize Ricatti variables S2, S1, S0
-		xt::xarray<double> S_2_zeros =
-			xt::zeros<double>({size_t(num_steps - 1), state_size, state_size});
-		xt::xarray<double> S_2_T = Q_f;
-		S_2_T.reshape({size_t(1), state_size, state_size});
-		auto S_2 = xt::concatenate(xtuple(S_2_zeros, S_2_T), 0);
-
-		xt::xarray<double> S_1_zeros = xt::zeros<double>({size_t(num_steps - 1), state_size});
 		xt::xarray<double> X_bar_d =
-			xt::expand_dims(xt::view(X_bar_desired, num_steps, xt::all()), 0);
-
+			xt::expand_dims(xt::view(X_bar_desired, num_steps, xt::all()), 1);
 		xt::xarray<double> X_bar_d_transp = xt::transpose(X_bar_d, {1, 0});
 
-		std::cout << Q_f << std::endl;
-		std::cout << X_bar_d << std::endl;
-		xt::xarray<double> S_1_T = -2.0 * xt::linalg::dot(Q_f, X_bar_d_transp);
-		xt::xarray<double> S_1 = xt::concatenate(xtuple(S_1_zeros, S_1_T), 0);
+		// Initialize Ricatti variables S2
+		std::vector<size_t> shape = {num_steps, state_size, state_size};
+		xt::xarray<double> S_2 = xt::zeros<double>(shape);
+		auto S_2_T = xt::view(S_2, num_steps-1, xt::all(), xt::all());
+		S_2_T = Q_f;
 
-		xt::xarray<double> S_0_zeros = xt::zeros<double>({num_steps - 1, size_t(1)});
-		xt::xarray<double> S_0_T_left = xt::linalg::dot(X_bar_d, Q_f);
-		xt::xarray<double> S_0_T = xt::linalg::dot(S_0_T_left, X_bar_d_transp);
-		xt::xarray<double> S_0 = xt::concatenate(xtuple(S_0_zeros, S_0_T), 0);
+		std::cout << S_2 << std::endl;
 
-		// std::cout << S_2 << std::endl;
-		// std::cout << S_0 << std::endl;
-		// std::cout << S_1 << std::endl;
+		// Initialize Ricatti variable S1
+		shape = {num_steps, state_size};
+		xt::xarray<double> S_1 = xt::zeros<double>(shape);
+		auto S_1_T = xt::view(S_1, num_steps-1, xt::all());
+		S_1_T = xt::squeeze(-2.0 * xt::linalg::dot(Q_f, X_bar_d));
+
+		std::cout << S_1 << std::endl;
+
+		// Initialize Ricatti variable S0
+		shape = {num_steps};
+		xt::xarray<double> S_0 = xt::zeros<double>(shape);
+		auto S_0_T = xt::view(S_0, num_steps-1);
+		xt::xarray<double> temp = xt::linalg::dot(X_bar_d_transp, Q_f);
+		S_0_T = xt::squeeze(xt::linalg::dot(temp, X_bar_d));
+
+		std::cout << S_0 << std::endl;
 
 		// Solve Ricatti backwards in time
-		for(size_t t = num_steps; t > 0; --t)
+		for(size_t t = num_steps-1; t > 0; --t)
 		{
 			// Get our current Ricatti Variables
-			xt::xarray<double> current_S_2 = xt::view(S_2, t, xt::all(), xt::all());
-			xt::xarray<double> current_S_1 = xt::view(S_1, t, xt::all());
-			xt::xarray<double> current_S_0 = xt::view(S_0, t, size_t(1));
-			xt::xarray<double> current_X_nominal = xt::view(X_nominal, t, xt::all());
-
+			xt::xarray<double> curr_S_2 = xt::view(S_2, t, xt::all(), xt::all());
+			xt::xarray<double> curr_S_1 = xt::view(S_1, t, xt::all());
+			xt::xarray<double> curr_S_0 = xt::view(S_0, t);
+			xt::xarray<double> curr_X_nominal = xt::view(X_nominal, t, xt::all());
+			
+			std::cout << curr_S_2 << std::endl;
+			std::cout << curr_S_1 << std::endl;
+			std::cout << curr_S_0 << std::endl;
+			std::cout << curr_X_nominal << std::endl;
+			
 			// Linearize our system dynamics
-			auto A_t = jacobian(current_X_nominal);
+			auto A_t = jacobian(curr_X_nominal);
 			xt::xarray<double> B_t {0, 0, 0, 1, 1};
 			B_t.reshape({5, 1});
 
